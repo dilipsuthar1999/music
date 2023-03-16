21e6086282b82b59a084043d1a9e7e9b31387ed2
dilipsuthar1999 <61174714+dilipsuthar1999@users.noreply.github.com>
2023-03-16 11:41:44 -0700

    Music Website Landing Page

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var slider = tns({
+    "container": '.reviews',
+    "responsive": {
+        "350": {
+            "items": 1,
+        },
+        "600": {
+            "items": 2
+        },
+        "992": {
+            "items": 3
+        }
+    },
+    "swipeAngle": false,
+    "speed": 400
+});
